ee9c6df28489e61a9c14e113b108b252f0fba3be
helloHKTK <1227553789@qq.com>
2017-08-09 07:55:37 +0800

    首次创建test01.php

---
+++ test01.php
@@ -0,0 +1,15 @@
+<?php
+  public class Student{
+    private $stuName;
+    public function setName($name)
+    {
+      $this->stuName=$name;
+    }
+    pulic function getName()
+    {
+      return $this->stuName;
+    }
+  } 
+  
+  
+?>
