5fc75ed30d2ca7ba014164f6990f2c5ddaa870f2
helloHKTK <1227553789@qq.com>
2017-08-08 23:41:57 +0800

    修改test01.php文件
    这是第一次修改！

--- test01.php
+++ test01.php
@@ -1,3 +1,18 @@
 <?php
-  phpinfo();
+ class Student{
+    private $stuName;
+    public function getName()
+    {
+        return $this->stuName;
+    }
+    public function setName($name)
+    {
+        $this->stuName=$name;
+    }
+}
+
+$s1 = new Student();//实例化一个对象
+$s1->setName("王五");
+printf("学生的姓名是：%1\$s",$s1->getName());
+
 ?>
